9f43aa47522d59147d44c84d7cb7bb768ea36c99
Emanuele Cesena <emanuelecesena@pinterest.com>
2020-06-14 10:46:46 -0700

    Beta

---
+++ admin/precache-manifest.b7d739d8b349a6d82b1b7c0ea025f372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f1cda746d7a05072c5e6d0232c2554",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "61533c46000e136e641e",
+    "url": "/admin/static/css/2.0e8af8c4.chunk.css"
+  },
+  {
+    "revision": "61533c46000e136e641e",
+    "url": "/admin/static/js/2.e98db946.chunk.js"
+  },
+  {
+    "revision": "ff4544b9e93c89e6c71923de61b52ce1",
+    "url": "/admin/static/js/2.e98db946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1398e8590d289919609e",
+    "url": "/admin/static/js/main.134fc712.chunk.js"
+  },
+  {
+    "revision": "3fb88b6516e58f76d50c",
+    "url": "/admin/static/js/runtime-main.24666479.js"
+  }
+]);
